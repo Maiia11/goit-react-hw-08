30f07d683006ee936b428bc4da657ee370cd0c7d
Maiia11 <149806655+Maiia11@users.noreply.github.com>
2024-06-29 10:35:27 +0300

    add all structure files

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,14 @@
 import { configureStore} from "@reduxjs/toolkit";
 import { contactsReducer } from "./contacts/slice";
 import { filtersReducer } from "./filters/slice";
+import { authReducer } from "./auth/slice";
 
 
 
 const rootReducer = {
   contacts: contactsReducer,
   filters: filtersReducer,
+  auth: authReducer
 };
 
 
