b224862894e6ddb8eedf91a645405995799a73fd
Maiia11 <149806655+Maiia11@users.noreply.github.com>
2024-06-30 11:29:45 +0300

    add icon

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,11 @@
-
+import css from "./HomePage.module.css"
+import { MdOutlineContactPhone } from "react-icons/md";
 
 const HomePage = () => {
   return (
     <div>
-      <h1>Contact meneger welcome to page</h1>
+      <h1 className={css.text}>Contact meneger welcome page</h1>
+      <MdOutlineContactPhone />
     </div>
   )
 }
